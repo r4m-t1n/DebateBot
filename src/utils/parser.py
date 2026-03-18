fef2585579ef1144561713fd79d89af2138bfdeb
r4m-t1n <im.r4mt1n@gmail.com>
2026-03-18 22:33:51 +0100

    feat: add parse_subjects, export_subjects

--- src/utils/parser.py
+++ src/utils/parser.py
@@ -1,3 +1,5 @@
+from utils.redis_utils import get_raw_subjects
+
 def split_args(text: str):
     args = list()
 
@@ -36,6 +38,44 @@ def split_args(text: str):
 
 def check_triggers(text: str, triggers: list[str]):
     for trigger in triggers:
-        if trigger in text:
+        if trigger.lower() in text.lower():
             return True
     return False
+
+def parse_subjects(subjects: list[list]):
+    unique_subjects = set()
+
+    for level_class in subjects:
+        for subject in level_class:
+            if subject:
+                unique_subjects.add(subject)
+    if not unique_subjects:
+            return "No subjects registered."
+
+    subjects_text = str()
+    for subject in unique_subjects:
+        subjects_text += f"- {subject}\n"
+
+    return subjects_text
+
+async def export_subjects():
+    subjects = list()
+
+    crtiticisms = await get_raw_subjects("criticism")
+    defendants = await get_raw_subjects("defendant")
+
+    for level in range(1, 6):
+        for subject in crtiticisms[level-1]:
+            subjects.append(
+                "- Subject: {} | Mode: criticism | Level: {}".format(
+                    subject, level
+                )
+            )
+    
+        for subject in defendants[level-1]:
+            subjects.append(
+                "- Subject: {} | Mode: defendant | Level: {}".format(
+                    subject, level
+                )
+            )
+    return subjects
